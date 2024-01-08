abc5e522ee89195b47d26de0eca8603ef4f2af5f
saumya <saumyaagarwal65@gmail.com>
2024-01-08 21:00:05 +0530

    Control topic, limit news, set language, add links, fix errors

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 from send_email import send_email
 
+topic="cars"
 api="c414ec4eb6ff4517b78d751a39b3f4a7"
-url= ("https://newsapi.org/v2/everything?q=tesla&from=2023-12-07&sortBy="
-      "publishedAt&apiKey=c414ec4eb6ff4517b78d751a39b3f4a7")
+url= (f"https://newsapi.org/v2/everything?q={topic}&sortBy="
+      "publishedAt&apiKey=c414ec4eb6ff4517b78d751a39b3f4a7&language=en")
 
 # make request
 request=requests.get(url)
@@ -13,12 +14,13 @@ content=request.json()
 
 message=""
 # access article titles and descriptions
-for article in content["articles"]:
-    if article["title"]:
-        message=message+article["title"]+"\n"+article["description"]+2*"\n"
+for article in content["articles"][:20]:
+    if article["title"] and article["description"]:
+        message=(message+article["title"]+"\n"+article["description"]
+                 +"\n" + article["url"] +2*"\n")
 
 final_msg=f"""\
-Subject: News
+Subject: News Today
 {message}
 """
 final_msg=final_msg.encode("utf-8")
