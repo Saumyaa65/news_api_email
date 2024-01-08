09a1271fde1f398f5a41a6fbc109b4bdc32e83cd
saumya <saumyaagarwal65@gmail.com>
2024-01-08 23:10:06 +0530

    Protecting app password and api

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 from send_email import send_email
+import os
 
 topic="cars"
-api="c414ec4eb6ff4517b78d751a39b3f4a7"
+api=os.getenv("API")
 url= (f"https://newsapi.org/v2/everything?q={topic}&sortBy="
-      "publishedAt&apiKey=c414ec4eb6ff4517b78d751a39b3f4a7&language=en")
+      f"publishedAt&apiKey={api}&language=en")
 
 # make request
 request=requests.get(url)
--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import os
 
 def send_email(message):
 
@@ -6,7 +7,7 @@ def send_email(message):
     port=465
 
     username="keepstudying65@gmail.com"
-    password="tznzpxbnenpzosrb"
+    password=os.getenv("PASSWORD")
     receiver="saumyaagarwal65@gmail.com"
     context= ssl.create_default_context()
 
